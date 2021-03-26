ab0b8aff5b37cf9acb019f4dd991920f41982585
Varun487 <seshuvarun@gmail.com>
2021-03-26 22:01:30 +0530

    Modified deploy script

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -1,13 +1,13 @@
 echo "Starting new deployment..."
 git checkout --orphan gh-pages
 echo "Building..."
-npm run  --prefix ../politicians-twitter-sentiment-ranking-ui/ build
-git --work-tree ../politicians-twitter-sentiment-ranking-ui/dist add --all
+npm run  --prefix ./politicians-twitter-sentiment-ranking-ui/ build
+git --work-tree ./politicians-twitter-sentiment-ranking-ui/dist add --all
 echo "Commit new changes..."
-git --work-tree ../politicians-twitter-sentiment-ranking-ui/dist commit -m 'Deploy'
+git --work-tree ./politicians-twitter-sentiment-ranking-ui/dist commit -m 'Deploy'
 echo "Pushing to gh-pages..."
 git push origin HEAD:gh-pages --force
-rm -r ../politicians-twitter-sentiment-ranking-ui/dist
+rm -r ./politicians-twitter-sentiment-ranking-ui/dist
 git checkout -f main
 git branch -D gh-pages
 echo "Successfully deployed!"
